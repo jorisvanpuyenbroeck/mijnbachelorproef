4889bb6c5de9e8b509202b3a0ebecc5c240fd9b4
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2025-01-27 15:58:19 +0100

    env tweak

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,21 @@
+// // environment.ts
+// export const environment = {
+//   production: false, // this is true in the production environment
+//   AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
+//   AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
+//   AUTH0_AUDIENCE: "https://localhost:7026", 
+//   redirectUri: "https://localhost:4200/callback",
+//   api_url: "https://gwbpapi.azurewebsites.net/api/",
+//   home_url: "http://localhost:4200",
+// };
+
 // environment.ts
 export const environment = {
-  production: false, // this is true in the production environment
+  production: true, // this is true in the production environment
   AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
   AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
-  AUTH0_AUDIENCE: "https://localhost:7026", 
-  redirectUri: "https://localhost:4200/callback",
+  AUTH0_AUDIENCE: "https://gwbpapi.azurewebsites.net/",
+  redirectUri: "https://lively-glacier-082d11d03.4.azurestaticapps.net//callback",
   api_url: "https://gwbpapi.azurewebsites.net/api/",
-  home_url: "http://localhost:4200",
+  home_url: "https://lively-glacier-082d11d03.4.azurestaticapps.net/",
 };
-
