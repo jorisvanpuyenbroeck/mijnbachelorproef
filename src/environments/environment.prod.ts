0bf663ed2d044b7e19ac2e4c905c6ddf742d263a
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2025-01-27 15:59:45 +0100

    slash teveel in callback url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
   AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
   AUTH0_AUDIENCE: "https://gwbpapi.azurewebsites.net/",
-  redirectUri: "https://lively-glacier-082d11d03.4.azurestaticapps.net//callback",
+  redirectUri: "https://lively-glacier-082d11d03.4.azurestaticapps.net/callback",
   api_url: "https://gwbpapi.azurewebsites.net/api/",
   home_url: "https://lively-glacier-082d11d03.4.azurestaticapps.net/",
 };
