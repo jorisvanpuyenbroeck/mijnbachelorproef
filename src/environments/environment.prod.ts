d5da7ace07b0011bf5e1877ca5551296a583cf9a
jorisvanpuyenbroeck <m9407046@student.thomasmore.be>
2025-01-27 16:15:13 +0100

    poging 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 // environment.ts
 export const environment = {
   production: true, // this is true in the production environment
-  AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
-  AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
+  AUTH0_DOMAIN: "mijnbachelorproef.eu.auth0.com",
+  AUTH0_CLIENT_ID: "DoC3fmUZ8j0Caomgjn7QPb2tDceVDPnt",
   AUTH0_AUDIENCE: "https://gwbpapi.azurewebsites.net/",
   redirectUri: "https://lively-glacier-082d11d03.4.azurestaticapps.net/callback",
   api_url: "https://gwbpapi.azurewebsites.net/api/",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 // // environment.ts
 // export const environment = {
 //   production: false, // this is true in the production environment
-//   AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
-//   AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
+//   AUTH0_DOMAIN: "mijnbachelorproef.eu.auth0.com",
+//   AUTH0_CLIENT_ID: "DoC3fmUZ8j0Caomgjn7QPb2tDceVDPnt",
 //   AUTH0_AUDIENCE: "https://localhost:7026", 
 //   redirectUri: "https://localhost:4200/callback",
 //   api_url: "https://gwbpapi.azurewebsites.net/api/",
@@ -12,8 +12,8 @@
 // environment.ts
 export const environment = {
   production: true, // this is true in the production environment
-  AUTH0_DOMAIN: "dev-toowwl21qlfjeu6w.eu.auth0.com",
-  AUTH0_CLIENT_ID: "U9OMeyZVRGhl9L0uS7A7jwvuDCYW0ca9",
+  AUTH0_DOMAIN: "mijnbachelorproef.eu.auth0.com",
+  AUTH0_CLIENT_ID: "DoC3fmUZ8j0Caomgjn7QPb2tDceVDPnt",
   AUTH0_AUDIENCE: "https://gwbpapi.azurewebsites.net/",
   redirectUri: "https://lively-glacier-082d11d03.4.azurestaticapps.net/callback",
   api_url: "https://gwbpapi.azurewebsites.net/api/",
